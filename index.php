537a0fbc3da84179c982912610c752c998619f2e
Shamilshiras <122472013+Shamilshiras@users.noreply.github.com>
2025-10-16 22:31:37 +0530

    login

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Selection</title>
+    <link rel="stylesheet" href="style_index.css">
+</head>
+<body>
+
+    <div class="container">
+        <h2>Login as</h2>
+        <button onclick="location.href='student_login.php'">Student</button>
+        <button onclick="location.href='teacher_login.php'">Teacher</button>
+    </div>
+
+</body>
+</html>
